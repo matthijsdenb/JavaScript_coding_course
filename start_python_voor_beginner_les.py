dbfa3f921b7d95fba669f69f4a55f4117de7b21f
Matthijs den Boer <matthijsdenboer@matthijss-air.home>
2022-07-06 15:49:20 +0200

    voor de zekerheid

--- start_python_voor_beginner_les.py
+++ start_python_voor_beginner_les.py
@@ -1,36 +1,48 @@
-print("hij werkt")
-
 
 # variable verbose
-
-ikkieszelf = 8
-print(ikkieszelf)
+#ikkieszelf = 8
+# print(ikkieszelf)
 
 # operator: = assign
 # wiskundig: + / * - % // **
 # strings = een stuk tekst
-variable2 = 7
-variable2 = ikkieszelf
-variable3 = 8+6*ikkieszelf
-print(variable3)
+# comperative operations:
 
-print(10**4)
-variable4 = 'Maandag'
-print(variable4)
+# variable2 = 7
+# variable2 = ikkieszelf
+# variable3 = 8+6*ikkieszelf
+# print(variable3)
 
-variable5 = ', Dinsdag'
-print(variable4+variable5)
-print(variable4*5)
+# print(10**4)
+#variable4 = 'Maandag'
+# print(variable4)
 
-var1 = "Kees"
-var2 = 13
+#variable5 = ', Dinsdag'
+# print(variable4+variable5)
+# print(variable4*5)
+
+#var1 = "Kees"
+#var2 = 13
 
 # 'casten': str()
-print(var1+" is mijn zoon en hij is oud: "+str(var2))
-var3 = "15"
-var4 = "17"
-print(int(var3) + int(var4))
+# print(var1+" is mijn zoon en hij is oud: "+str(var2))
+# var3 = "15"
+# var4 = "17"
+# print(int(var3) + int(var4))
 
-getal1 = input("geef getal 1")
-getal2 = input("geef getal 2")
-print(int(getal1) + int(getal2))
+# getal1 = input("geef getal 1")
+# getal2 = input("geef getal 2")
+# print(int(getal1) + int(getal2))
+
+# belangrijke statements: if elif else for
+getal5 = 5
+if getal5 == 5:
+    print("Ja het is vijf")
+else:
+    print("Nee is het geen vijf")
+
+
+# operator: = assign
+# wiskundig: + / * - % // **
+# strings = een stuk tekst, kan gebruikt worden bij * of +
+# comperative operations: ==
