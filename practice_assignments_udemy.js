59630acedb4fc6c89432abdc36bd80bddcf8ca57
Matthijs den Boer <matthijsdenboer@matthijss-air.home>
2022-07-06 15:42:21 +0200

    toevoegen beginner les

--- practice_assignments_udemy.js
+++ practice_assignments_udemy.js
@@ -1,4 +1,7 @@
 // All comments are named as follows: Practice Assignments (PA 1 - Lecture Name - Date) 
+
+// All 
+
 // PA 1 - Values & Variables - 06.07.22 
 let country = "Netherlands";
 let continent = "Europe";
@@ -8,3 +11,4 @@ console.log(country);
 console.log(continent);
 console.log(population);
 
+// PA 2 - Data Types - 06.07.22
---
+++ start_python_voor_beginner_les.py
@@ -0,0 +1,36 @@
+print("hij werkt")
+
+
+# variable verbose
+
+ikkieszelf = 8
+print(ikkieszelf)
+
+# operator: = assign
+# wiskundig: + / * - % // **
+# strings = een stuk tekst
+variable2 = 7
+variable2 = ikkieszelf
+variable3 = 8+6*ikkieszelf
+print(variable3)
+
+print(10**4)
+variable4 = 'Maandag'
+print(variable4)
+
+variable5 = ', Dinsdag'
+print(variable4+variable5)
+print(variable4*5)
+
+var1 = "Kees"
+var2 = 13
+
+# 'casten': str()
+print(var1+" is mijn zoon en hij is oud: "+str(var2))
+var3 = "15"
+var4 = "17"
+print(int(var3) + int(var4))
+
+getal1 = input("geef getal 1")
+getal2 = input("geef getal 2")
+print(int(getal1) + int(getal2))
