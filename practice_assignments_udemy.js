799a202292a9769c56c24a8132a1c0e36c5d0ab5
Matthijs den Boer <matthijsdenboer@matthijss-air.home>
2022-07-06 14:15:44 +0200

    add practice assignments

---
+++ practice_assignments_udemy.js
@@ -0,0 +1,10 @@
+// All comments are named as follows: Practice Assignments (PA 1 - Lecture Name - Date) 
+// PA 1 - Values & Variables - 06.07.22 
+let country = "Netherlands";
+let continent = "Europe";
+let population = "16 million";
+
+console.log(country);
+console.log(continent);
+console.log(population);
+
